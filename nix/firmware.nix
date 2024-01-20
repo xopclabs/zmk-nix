09e879acd7c58232b8fc5bac27a3ab78e6e2d012
xopclabs <github@conv2d.com>
2024-01-20 07:01:23 +0400

    Update for cradio

--- nix/firmware.nix
+++ nix/firmware.nix
@@ -4,17 +4,17 @@
 }:
 
 buildSplitKeyboard {
-  name = "sofle-firmware";
+  name = "cradio-firmware";
 
   src = lib.sourceFilesBySuffices self [ ".conf" ".keymap" ".yml" ];
 
-  board = "nice_nano_v2";
-  shield = "sofle_%PART% nice_view_adapter nice_view";
+  board = "nrfmicro_13";
+  shield = "cradio_%PART%";
 
   zephyrDepsHash = "sha256-u7zdkwXerCmL9OOI3/v2czIb8Vfr1yx8wuK2IpIdzv0=";
 
   meta = with lib; {
-    description = "Keyboard firmware for Sofle RGB with nice!view screens";
+    description = "Keyboard firmware for Ferris Sweep";
     license = licenses.mit;
     platforms = platforms.all;
     maintainers = with maintainers; [ lilyinstarlight ];
