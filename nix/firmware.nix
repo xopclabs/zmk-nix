92c09ebb024a3a05ff2c53dd3500353129dc2347
xopclabs <github@conv2d.com>
2024-02-04 20:52:15 +0400

    Updated hash

--- nix/firmware.nix
+++ nix/firmware.nix
@@ -11,7 +11,7 @@ buildSplitKeyboard {
   board = "nrfmicro_13";
   shield = "cradio_%PART%";
 
-  zephyrDepsHash = "";
+  zephyrDepsHash = "sha256-4R8NUx+VRz/EiqYuEGtQqidVkI8z6KxFHYoQK7twe9o=";
 
   meta = with lib; {
     description = "Keyboard firmware for Ferris Sweep";
